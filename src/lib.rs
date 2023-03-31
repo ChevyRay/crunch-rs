2e58144d0feab635f846f5a21f7026b7251297c0
Chevy Ray Johnston <happytrash@gmail.com>
2023-03-31 13:27:41 -0700

    update readme

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 A rectangle packer, written in Rust, for cramming lots of rectangles into a larger one. It is designed
 primarily with sprite-packing in mind (eg. to create sprite-atlases or CSS image sheets).
 
-Supports 90º rotation on a per-item basis.
+Supports 90° rotation on a per-item basis.
 
 ![image of packed rectangles](https://i.imgur.com/Xa3Oi9c.png)
 > *1200 rectangles packed with ~99% coverage*
